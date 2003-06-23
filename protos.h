dd85af6d34bff60076230a6f6a669d71ad043afd
Petter Reinholdtsen <pere@debian.org>
2003-06-23 23:11:23 +0000

    Avoid non-standard __WORDSIZE.
    git-svn-id: svn://svn.debian.org/svn/chrpath/trunk@88 0b574128-1223-0410-84d9-e5205181c9b3

--- protos.h
+++ protos.h
@@ -9,22 +9,21 @@
 #else
 #define ELFDATA2 ELFDATA2LSB
 #endif
-#if __WORDSIZE == 64
+#if SIZEOF_VOID_P == 8
 #define Elf_Ehdr Elf64_Ehdr
 #define ELFCLASS ELFCLASS64
 #define Elf_Phdr Elf64_Phdr
 #define Elf_Shdr Elf64_Shdr
 #define Elf_Dyn  Elf64_Dyn
-#elif __WORDSIZE == 32
+#elif SIZEOF_VOID_P == 4
 #define Elf_Ehdr Elf32_Ehdr
 #define ELFCLASS ELFCLASS32
 #define Elf_Phdr Elf32_Phdr
 #define Elf_Shdr Elf32_Shdr
 #define Elf_Dyn  Elf32_Dyn
 #else
-#error "Unknown __WORDSIZE!"
+#error "Unknown word size (SIZEOF_VOID_P)!"
 #endif
-
 
 int killrpath(const char *filename);
 int chrpath(const char *filename, const char *newpath, int convert);
@@ -34,6 +33,5 @@ void elf_close(int fd);
 int elf_find_dynamic_section(int fd, Elf_Ehdr *ehdr, Elf_Phdr *phdr);
 const char *elf_tagname(int tag);
 int elf_dynpath_tag(int tag);
-
 
 #endif /* PROTOS_H */
